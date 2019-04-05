64eb60c260fc07355cda3165fdca7dbf04e486cb
XiaoqiangWu <xiaoqiangwu.hit@gmail.com>
2019-04-05 16:53:05 +0800

    using use-package

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -8,14 +8,8 @@
 ;;| |/ / ____\ \ / / __ )_ _| \ | |  _ \_ _| \ | |/ ___/ ___| 
 ;;| ' /|  _|  \ V /|  _ \| ||  \| | | | | ||  \| | |  _\___ \ 
 ;;| . \| |___  | | | |_) | || |\  | |_| | || |\  | |_| |___) |
-;;|_|\_\_____| |_| |____/___|_| \_|____/___|_| \_|\____|____/ 
+;;|_|\_\_____| |_| |____/___|_| \_|____/___|_| \_|\____|____/
 
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h f") 'counsel-describe-function)
-(global-set-key (kbd "C-h v") 'counsel-describe-variable)
 
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
@@ -26,34 +20,12 @@
 
 (global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
 
-(global-set-key (kbd "s-/") 'hippie-expand)
-
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-(global-set-key (kbd "C-c p f") 'counsel-git)
-
-(global-set-key (kbd "C-c p s") 'helm-do-ag-project-root)
-
-(global-set-key (kbd "M-s e") 'iedit-mode)
 ;; r refers to remeber
 (global-set-key (kbd "C-c r") 'org-capture)
 
 (global-set-key (kbd "C-c a") 'org-agenda)
 ;; Enable Cache
 (setq url-automatic-caching t)
-
-;; Example Key binding
-(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
-
-;; Integrate with popwin-el (https://github.com/m2ym/popwin-el)
-;; (push "*Youdao Dictionary*" popwin:special-display-config)
-
-;; Set file path for saving search history
-;; (setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao")
-
-;; 主动加载 Dired Mode
-;; (require 'dired)
-;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 
 ;; 延迟加载
 (with-eval-after-load 'dired
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -5,29 +5,136 @@
 ;;|___|_| \_|___| |_|      |_| /_/   \_\____|_|\_\/_/   \_\____|_____|____/ 
 
 
-(global-hungry-delete-mode)
+(use-package hungry-delete
+  :ensure t
+  :config
+  (global-hungry-delete-mode))
 
-;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-;;(smartparens-global-mode t)
+(use-package ivy
+  :ensure t
+  :bind ("C-c C-r" . 'ivy-resume)
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t))
 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.08)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 1)
+  (setq company-tooltip-align-annotations t)
+  ;; invert the navigation direction if the the completion popup-isearch-match
+  ;; is displayed on top (happens near the bottom of windows)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
 
-(global-company-mode t)
+(use-package company-coq
+  :ensure t
+  :config
+  (add-hook 'coq-mode-hook #'company-coq-mode))
 
-(load-theme 'monokai t)
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
 
-;;(add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'coq-mode-hook 'flycheck-mode)
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'coq-mode-hook 'flycheck-mode))
 
-(require 'popwin)
-(popwin-mode t)
+(use-package popwin
+  :ensure t
+  :config
+  (popwin-mode t))
 
-;; Load company-coq when opening Coq files
-(add-hook 'coq-mode-hook #'company-coq-mode)
+(use-package youdao-dictionary
+  :ensure t
+  :bind ("C-c y" . 'youdao-dictionary-search-at-point)
+  :config
+  (setq youdao-dictionary-use-chinese-word-segmentation t))
 
-;; Enable Chinese word segmentation support (支持中文分词)
-(setq youdao-dictionary-use-chinese-word-segmentation t)
+(use-package org-pomodoro
+  :ensure t)
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package swiper
+  :ensure t
+  :bind ("\C-s" . 'swiper))
+
+(use-package counsel
+  :ensure t
+  :config
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c a") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package helm-ag
+  :ensure t
+  :bind ("C-c p s" . 'helm-do-ag-project-root))
+
+(use-package iedit
+  :ensure t
+  :bind ( "M-s e" . 'iedit-mode))
+
+(use-package winum
+  :init (setq winum-keymap
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd "C-`") 'winum-select-window-by-number)
+      (define-key map (kbd "C-²") 'winum-select-window-by-number)
+      (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+      (define-key map (kbd "M-1") 'winum-select-window-1)
+      (define-key map (kbd "M-2") 'winum-select-window-2)
+      (define-key map (kbd "M-3") 'winum-select-window-3)
+      (define-key map (kbd "M-4") 'winum-select-window-4)
+      (define-key map (kbd "M-5") 'winum-select-window-5)
+      (define-key map (kbd "M-6") 'winum-select-window-6)
+      (define-key map (kbd "M-7") 'winum-select-window-7)
+      (define-key map (kbd "M-8") 'winum-select-window-8)
+      map))
+  :ensure t
+  :config
+  (setq winum-auto-setup-mode-line nil)
+  (winum-mode))
+
+(use-package spaceline
+  :ensure t
+  :config
+  (spaceline-spacemacs-theme))
+
+(use-package proof-general
+  :ensure t)
+
+(use-package autopair
+  :ensure t
+  :config
+  (autopair-global-mode)
+  )
 
 (cua-mode t)
 (setq-default cua-auto-tabify-rectangles nil)
@@ -40,11 +147,4 @@
     (highlight-parentheses-mode t)))
 (global-highlight-parentheses-mode t)
 
-(require 'org-pomodoro)
-
-(require 'yasnippet)
-(yas-reload-all)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
-
-(which-key-mode)
 (provide 'init-packages)
